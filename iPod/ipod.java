1c5d777450fddf8f48e42c71ffcaa414fa70a006
Jessica Morrison <94693340+jessieM4664@users.noreply.github.com>
2023-10-11 12:42:35 -0400

    Update ipod.java

--- iPod/ipod.java
+++ iPod/ipod.java
@@ -1,1 +1,14 @@
-
+public class ipod {
+    
+    String songs[] = {"The Gambler",
+                      "Ice Ice Baby",
+                      "Beer For My Horses",
+                      "Would That I",
+                      "King Of The Road",
+                      "All I Want",
+                      "Night Moves",
+                      "By And By",
+                      "Spirit In The Sky",
+                      "Hot For Teacher"};
+}
+// manually add songs

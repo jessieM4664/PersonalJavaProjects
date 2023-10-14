09a24857ed5f18cd0bd3166d6f6f7e455cd42533
Jessica Morrison <94693340+jessieM4664@users.noreply.github.com>
2023-10-14 11:16:32 -0400

    Create GamePanel.java

---
+++ LibraryWormGame/GamePanel.java
@@ -0,0 +1,79 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
+
+public class GamePanel extends JPanel implements Runnable {
+    // Screen settings
+
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+
+    BasicComboBoxUI box = new BasicComboBoxUI();
+    KeyHandler keyH = box.new KeyHandler();
+    Thread gameThread; // keeps program running untill manually stopped
+
+    //set players default postion
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start(); // calls run method automaticaly
+    }
+
+    @Override
+    public void run() { // game loop
+        while(gameThread != null) {
+            //System.out.println("The game loop is running");
+            
+            // 1 : UPDATE avatar position
+            update();
+            // 2 : DRAW the screen with updated information
+            repaint();
+        }
+    }
+
+    public void update() {
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
+    }
+}
